e2815ca7064792a3dc60a5f3e1880d19b60c2422
jihye <wlgul817@gmail.com>
2024-11-21 17:57:24 +0900

    ✨ feat : 숫자 입력시 실행할 함수 작성

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function inputNumber() {
+  let gameInput = document.getElementById("gameInput").value;
+  if (gameInput == "1") {
+    console.log("게임 시작");
+    //게임 시작시 실행할 함수
+  } else if (gameInput == "9") {
+    console.log("게임 종료");
+  } else {
+    alert("1 또는 9를 입력하세요");
+  }
+}
